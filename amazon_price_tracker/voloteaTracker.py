761678bf998c48b877fdb4d07118f27c1eb56bf9
vittoriopipoli <vittoriopipoli@gmail.com>
2020-07-09 12:16:00 +0200

    updated

--- amazon_price_tracker/voloteaTracker.py
+++ amazon_price_tracker/voloteaTracker.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 
-URL = "https://www.amazon.co.uk/Apple-Airpods-Wireless-Charging-latest/dp/B07PYM8FB8/ref=sr_1_6?keywords=airpods&qid=1581244893&sr=8-6"
+URL = "https://www.volotea.com/it/offerte-voli/catania/bari/?adults=1&childs=0&infants=0&jorney=oneway"
 user_agent_list = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
@@ -27,30 +27,17 @@ def trackPrices():
 def getPrice():
     page = requests.get(URL, headers=HEADERS)
     print(page)
-    # soup = BeautifulSoup(page.content, 'html.parser')
-    # print(soup)
-    # print(page.content)
-
     soup = BeautifulSoup(page.content)
     print(soup)
-    # print(parsed_html.body.find('div', attrs={'id': 'productTitle'}).text)
-
     print("Trying to Parse Html...")
-    title = soup.find(id='productTitle')
-    # title = soup.select('productTitle')
+    title = soup.find(attrs={"data-date" : "2020-10-28"})
     if title is not None:
         title = title.get_text().strip()
-        # for i in title:
-        #     i = i.get_text().strip()
-        #     print(i)
 
-    price = soup.find(id='priceblock_ourprice')
-    # price = soup.select('priceblock_ourprice')
+    price = soup.find(attrs={"data-date" : "2020-07-30"})
     if price is not None:
-        price = price.get_text().strip()[1:4]
-        # for i in price:
-        #     i = i.get_text().strip()[1:4]
-        #     print(i)
+        price = price.get_text().strip()
+
     print(title)
     print(price)
     return price
@@ -67,4 +54,5 @@ def sendEmail():
     pass
 
 if __name__ == "__main__":
-    trackPrices()
+    # trackPrices()
+    getPrice()
